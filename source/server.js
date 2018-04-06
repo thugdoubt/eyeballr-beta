5997a44a46d67c14d6dfb094b3b04c7acf3b7359
nep <nep@noisetu.be>
2018-04-06 13:22:47 -0500

    add ping endpoint

--- source/server.js
+++ source/server.js
@@ -10,5 +10,10 @@ app.get('/ok', function(req, res) {
     res.send('eyeballr OK');
 });
 
+app.get('/api/ping', function(req, res) {
+    console.log('PONG');
+    res.send('eyeballr PONG');
+});
+
 app.listen(config.API_PORT, config.API_HOST);
 console.log(`running on http://${config.API_HOST}:${config.API_PORT}`);
