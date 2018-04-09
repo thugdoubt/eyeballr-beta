06966326a769421b26f001e4ae74f5be23dc62e0
nep <nep@noisetu.be>
2018-04-09 11:28:48 -0500

    refactor upload, add more faces

--- dev/upload.py
+++ dev/upload.py
@@ -7,17 +7,23 @@ import click
 from pprint import pprint as pp
 
 @click.command()
-@click.argument('filename')
-@click.argument('url')
-def main(filename, url):
-    with open(filename, 'rb') as f:
-        content = f.read()
-        data = ('data:image/png;base64,' + content.encode('base64')).replace('\n', '')
-        jar = requests.cookies.RequestsCookieJar()
-        jar.set('UID', '70ac51ed-d34d-417e-887c-64cd1edd3c42')
-        res = requests.post(url, json={'filename': filename, 'data': data}, cookies=jar)
-        pp(res)
-        pp(res.content)
+@click.argument('url', nargs=1)
+@click.argument('filename', nargs=-1)
+def main(url, filename):
+    jar = requests.cookies.RequestsCookieJar()
+    for fn in filename:
+        print('uploading {}'.format(fn))
+        with open(fn, 'rb') as f:
+            content = f.read()
+            data = ('data:image/png;base64,' + content.encode('base64')).replace('\n', '')
+            res = requests.post(url, json={'filename': fn, 'data': data}, cookies=jar)
+            jar = res.cookies
+            pp(res)
+            pp(res.content)
+    print('animating!')
+    res = requests.post(url.replace('upload', 'merge'), cookies=jar)
+    pp(res)
+    pp(res.content)
 
 if __name__ == '__main__':
     main()
